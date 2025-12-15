22d4e128bb445793c36f7f46951921dd0cbcd190
yzh <2575295717@qq.com>
2025-12-15 13:53:50 +0800

    第一章的代码关于CMakeList编译cpp代码的实例

---
+++ chapt1/CMakeFiles/learn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn.dir/hello.cpp.o"
+  "CMakeFiles/learn.dir/hello.cpp.o.d"
+  "learn"
+  "learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
